bbdbbbf270008dbeb40fd71dcd5e72ab39b2ce53
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-01 00:56:38 -0300

    Criação da conexão com o banco

--- src/dao/ConexaoFactory.java
+++ src/dao/ConexaoFactory.java
@@ -1,5 +1,24 @@
 package dao;
 
-public class ConexaoFactory{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConexaoFactory{
+    private static final String url = "jdbc:postgresql://localhost:5432/sistema-agendamento";
+    private static final String user = "postgres";
+    private static final String password = "1234";
+
+    public static Connection conectar(){
+        try{
+            Connection conexaoGerada = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Banco conectado com sucesso!");
+            return conexaoGerada;
+        }catch(SQLException e){
+            System.out.println("Erro: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
