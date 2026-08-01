8d0a85829b5749c0d0bbb7f65ee9a1fa544cc94d
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-01 17:25:47 -0300

    Tabelas do banco de dados do 'SISTEMA DE AGENDAMENTO'

--- src/dao/schema.sql
+++ src/dao/schema.sql
@@ -1,5 +1,5 @@
 create table clientes(
-	id int primary key serial,
+	id serial primary key,
 	nome varchar(100) not null,
 	cpf varchar(14) unique not null,
 	telefone varchar(20) not null,
@@ -12,7 +12,7 @@ create table esportes(
 );
 
 create table quadras(
-	id int primary key serial,
+	id serial primary key,
 	nome varchar(50) not null,
 	coberta boolean default false,
 	ativa boolean default true
@@ -25,16 +25,16 @@ create table quadras_esportes(
 	primary key(quadra_id, esporte_id),
 
 	foreign key(quadra_id) references quadras(id) on delete cascade,
-	foreihn key(esporte_id) references esportes(id) on delete cascade,
+	foreign key(esporte_id) references esportes(id) on delete cascade
 );
 
 create table tarifas(
-	id int primary key serial,
+	id serial primary key,
 	quadra_id int not null,
 	dia_semana int not null,
 	hora_inicio time not null,
 	hora_fim time not null,
-	preco_hora decimal(10,2) not null
+	preco_hora decimal(10,2) not null,
 
 	foreign key (quadra_id) references quadras(id),
 	constraint checagem_horario check (hora_fim > hora_inicio)
@@ -49,7 +49,7 @@ create table reservas(
 	horario_inicio time not null,
 	horario_fim time not null,
 	valor_total decimal(10,2) not null,
-	status varchar(20) default 'CONFIRMADA' -- 'CONFIRMADA', 'CANCELADA', 'FINALIZADA',
+	status varchar(20) default 'CONFIRMADA', -- 'CONFIRMADA', 'CANCELADA', 'FINALIZADA',
 	data_criacao timestamp default current_timestamp,
 
 	foreign key (cliente_id) references clientes(id),
