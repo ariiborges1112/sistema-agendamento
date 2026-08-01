655baeecd1c2a7cd99e12ac1b1f231e9346eb327
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-01 19:03:38 -0300

    Classe modelo Esporte para EsporteDAO

--- src/model/Quadra.java
+++ src/model/Quadra.java
@@ -6,39 +6,20 @@ public class Quadra{
     private boolean coberta;
     private boolean ativa;
 
-    public Quadra(String nome, boolean coberta, boolean ativa){
+    public Quadra(String nome){
         validarNome(nome);
         this.nome = nome;
-        validarCpf(cpf);
-        this.cpf = cpf;
-        validarTelefone(telefone);
-        this.telefone = telefone;
+
+        this.coberta = false;
+        this.ativa = true;
     }
 
     private void validarNome(String nome){
         if(nome == null || nome.trim().isEmpty()){ //not null
             throw new IllegalArgumentException("O nome não pode ser vazio");
         }
-        if(nome.length() > 100){ //varchar(100)
-            throw new IllegalArgumentException("O nome não pode ter mais que 100 caracteres");
-        }
-    }
-
-    private void validarCpf(String cpf){
-        if(cpf == null || cpf.trim().isEmpty()){ //not null
-            throw new IllegalArgumentException("O cpf não pode ser vazio");
-        }
-        if(cpf.length() > 14){ //varchar(14)
-            throw new IllegalArgumentException(("O cpf não pode ter mais que 14 caracteres"));
-        }
-    }
-
-    private void validarTelefone(String telefone){
-        if(telefone == null || telefone.trim().isEmpty()){ //not null
-            throw new IllegalArgumentException("O telefone não pode ser vazio");
-        }
-        if(telefone.length() > 20){ //varchar(20)
-            throw new IllegalArgumentException("O telefone não pode ter mais que 20 caracteres");
+        if(nome.length() > 100){ //varchar(50)
+            throw new IllegalArgumentException("O nome não pode ter mais que 50 caracteres");
         }
     }
 
