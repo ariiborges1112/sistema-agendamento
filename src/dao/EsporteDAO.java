3f69214b4b0c440a13bd584d1270dc1b8a74f275
ariiborges1112 <ari.borges1112@gmail.com>
2026-08-02 00:49:22 -0300

    funcionalidade INSERT concluida

--- src/dao/EsporteDAO.java
+++ src/dao/EsporteDAO.java
@@ -1,4 +1,25 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import model.Esporte;
+
 public class EsporteDAO{
+    public void salvar(Esporte esporte){
+        try{
+            String sql = "INSERT INTO esportes(nome) values (?)";
+
+            Connection conexao = ConexaoFactory.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+
+            stmt.setString(1, esporte.getNome());
+            stmt.executeUpdate();
+            stmt.close();
+            conexao.close();
+        }catch(SQLException e){
+            System.out.println("Erro: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
